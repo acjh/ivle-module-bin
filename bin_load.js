14fab0769e86620fb4e0f0066ce9a5d4f1ff1f3e
Aaron <aaronjhchong@hotmail.com>
2016-11-19 20:04:09 +0800

    Bin previously hidden modules

--- bin_load.js
+++ bin_load.js
@@ -5,9 +5,20 @@ function injectJS() {
   document.getElementsByTagName("head")[0].appendChild(script);
 }
 
+function binPreviouslyHidden() {
+  var modules = localStorage.getItem("hidden");
+  if (modules != null) {
+    modules = modules.split(" ");
+    for (var module in modules) {
+      $( ".col-md-7 > :contains(" + modules[module] + ")" ).parent().parent().parent().parent()[0].remove()
+    }
+  }
+}
+
 function insertBinIcons() {
   $( ".panel-body > .panel > .panel-heading > .row > .col-md-5" ).children().prepend("<a onclick='bin(this);'><span class='glyphicon glyphicon-trash'></span></a> &nbsp; ");
 }
 
 injectJS();
+binPreviouslyHidden();
 insertBinIcons();
