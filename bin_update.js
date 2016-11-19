33ca25bbae5a244929d3bed7ae365ddb70865e13
Aaron Chong <aaron.chong13@sps.nus.edu.sg>
2016-11-19 21:27:38 +0800

    Toggle to show all instead of page reload

--- bin_update.js
+++ bin_update.js
@@ -7,10 +7,20 @@ function rememberHidden(element) {
   var modules = localStorage.getItem("hidden");
   if (modules == null) {
     modules = "";
-    $( ".col-md-5 > :contains(Refresh Modules)" ).parent().children().prepend("<a onclick='localStorage.clear();window.location.reload();'>Show all</a> &nbsp; ");
+    $( ".col-md-5 > :contains(Refresh Modules)" ).parent().children().prepend("<a onclick='showAll();'>Show all</a> &nbsp; ");
   } else {
     modules += " ";
   }
   modules += $(element).parent().parent().prev().children().text().trim();;
   localStorage.setItem("hidden", modules);
+}
+
+function showAll() {
+  var modules = localStorage.getItem("hidden");
+  modules = modules.split(" ");
+  for (var module in modules) {
+    $( ".col-md-7 > :contains(" + modules[module] + ")" ).parent().parent().parent().parent().toggle(1000);
+  }
+  localStorage.clear();
+  $( "a:contains(Show all)" ).remove().parent().children().prepend("<a onclick='showAll();'>Show all</a> &nbsp; ");
 }
