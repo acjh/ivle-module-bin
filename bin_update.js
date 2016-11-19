af83c4d530f4a422ecdbba8ff45e52d628cb70ed
Aaron <aaronjhchong@hotmail.com>
2016-11-19 20:03:26 +0800

    Store hidden modules

--- bin_update.js
+++ bin_update.js
@@ -1,3 +1,15 @@
 function bin(element) {
+  rememberHidden(element);
   $(element).parent().parent().parent().parent().parent().remove();
+}
+
+function rememberHidden(element) {
+  var modules = localStorage.getItem("hidden");
+  if (modules == null) {
+    modules = "";
+  } else {
+    modules += " ";
+  }
+  modules += $(element).parent().parent().prev().children().text().trim();;
+  localStorage.setItem("hidden", modules);
 }
