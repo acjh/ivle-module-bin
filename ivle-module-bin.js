9a16e227643f3c3d3343b236606feda0549c9769
Aaron Chong <aaronjhchong@hotmail.com>
2017-09-10 11:30:22 +0800

    Move run-once functions to ivle-module-bin.js

--- ivle-module-bin.js
+++ ivle-module-bin.js
@@ -1,3 +1,27 @@
+(function () {
+  addBtnBin();
+  binHiddenModules();
+
+  /* run-once functions */
+
+  function addBtnBin() {
+    var selector = ".panel-body > .panel > .panel-heading > .row > .col-md-5 > .pull-right";
+    var popover = " " + "data-toggle='popover' data-placement='bottom' data-content='Bin' data-trigger='hover'";
+    var icon = "<span class='glyphicon glyphicon-trash'></span>";
+    $(selector).prepend("<a class='module-bin'" + popover + ">" + icon + "</a> &nbsp; ");
+    $('.module-bin').click(bin);
+    $('a[data-content="Bin"]').popover();
+  }
+
+  function binHiddenModules() {
+    var modules = getHiddenModules();
+    if (modules != null) {
+      addBtnShowAll();
+      togglePanels(modules, delay=0);
+    }
+  }
+})();
+
 /* 'onclick' functions */
 
 function bin() {
