96ff572021f6fd05795522b96d878a8fcb01d1c8
Aaron Chong <aaronjhchong@hotmail.com>
2017-09-11 15:32:38 +0800

    Workaround for notification count render too early

--- ivle-notifications.js
+++ ivle-notifications.js
@@ -1,5 +1,16 @@
 (function () {
+    addTemporaryStyle();
     document.addEventListener('DOMContentLoaded', hideNotificationCountWhen0, false);
+
+    var style;
+
+    function addTemporaryStyle() {
+        var css = '#notification_count { display: none; }';
+        style = document.createElement('style');
+        style.type = 'text/css';
+        style.appendChild(document.createTextNode(css));
+        document.documentElement.appendChild(style);
+    }
 
     function hideNotificationCountWhen0() {
         var count = document.getElementById("notification_count");
@@ -7,7 +18,12 @@
         var config = { childList: true };
         var observer = new MutationObserver(function (mutations) {
             count.hidden = target.innerText == 0;
+            removeTemporaryStyle();
         });
         observer.observe(target, config);
+    }
+
+    function removeTemporaryStyle() {
+        style.remove();
     }
 })();
